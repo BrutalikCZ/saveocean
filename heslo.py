b2ab58327470f7c0f59e0686a9f9564f4e7f0ecf
BrutalikCZ <tomas.brezina2006@gmail.com>
2024-11-23 22:20:25 +0100

    Testovací zkouška 1

---
+++ heslo.py
@@ -0,0 +1,1 @@
+passwordMySQL= "Heslo123"
---
+++ testing.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from heslo.py import passwordMySQL
+
+mydb = mysql.connector.connect(
+    host="localhost"
+    user="root"
+    password="passwordMySQL"
+    database ="saveOceanTest"
+)
+
